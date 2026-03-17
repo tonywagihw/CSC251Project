6c650c6240010b970577a0bc4dad2e1889e66aff
tonywagihwa <tkryako0@email.cpcc.edu>
2026-03-17 15:22:21 -0400

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -10,6 +10,7 @@ public class Policy
     private double weight;
 
     // No-arg constructor
+    
     public Policy()
     {
         policyNumber = 0;
@@ -23,6 +24,18 @@ public class Policy
     }
 
     // Constructor WITHOUT using this
+    
+    /**
+      * Constructor that initializes all policy fields.
+      * @param pNumber policy number
+      * @param pProvider provider name
+      * @param pFirst first name
+      * @param pLast last name
+      * @param pAge age
+      * @param pSmoking smoking status
+      * @param pHeight height in inches
+      * @param pWeight weight in pounds
+    */
     public Policy(int pNumber, String pProvider, String pFirst,
                   String pLast, int pAge, String pSmoking,
                   double pHeight, double pWeight)
@@ -38,22 +51,72 @@ public class Policy
     }
 
     // Getters
+    
+    /**
+      * Gets the policy number.
+      * @return the policy number
+    */
     public int getPolicyNumber() { return policyNumber; }
+    
+    /**
+      * Gets the provider name.
+      * @return the provider name
+    */
     public String getProviderName() { return providerName; }
-    public String getFirstName() { return firstName; }
+    
+    /**
+     * Gets the policyholder's first name.
+     * @return the first name
+   */
+   public String getFirstName() { return firstName; }
+    
+   /**
+      * Gets the policyholder's last name.
+      * @return the last name
+   */
     public String getLastName() { return lastName; }
+    
+    /**
+      * Gets the policyholder's age.
+      * @return the age
+   */
     public int getAge() { return age; }
+    
+    /**
+      * Gets the policyholder's smoking status.
+      * @return the smoking status
+    */
     public String getSmokingStatus() { return smokingStatus; }
+    
+    /**
+      * Gets the policyholder's height in inches.
+      * @return the height
+    */
     public double getHeight() { return height; }
+    
+    /**
+      * Gets the policyholder's weight in pounds.
+      * @return the weight
+    */
     public double getWeight() { return weight; }
 
     // BMI calculation
+    
+    /**
+      * Calculates the BMI of the policyholder.
+      * @return BMI value
+    */
     public double calculateBMI()
     {
         return (weight * 703) / (height * height);
     }
 
     // Price calculation
+    
+    /**
+      * Calculates the insurance price.
+      * @return total policy price
+    */
     public double calculatePrice()
     {
         double price = 600.0;

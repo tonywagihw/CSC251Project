c831e4f5728b1ce8b9f5ad8bcd3b49c89ab617c1
tonywagihwa <tkryako0@email.cpcc.edu>
2026-04-12 21:39:53 -0400

    Update PolicyHolder.java
    Added toString method

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -29,4 +29,16 @@ public class PolicyHolder
     {
         return (weight * 703) / (height * height);
     }
+    
+   @Override
+   public String toString()
+   {
+      return "Policyholder's First Name: " + firstName + "\n" +
+             "Policyholder's Last Name: " + lastName + "\n" +
+             "Policyholder's Age: " + age + "\n" +
+             "Policyholder's Smoking Status (smoker/non-smoker): " + smokingStatus + "\n" +
+             "Policyholder's Height: " + height + " inches\n" +
+             "Policyholder's Weight: " + weight + " pounds\n" +
+             String.format("Policyholder's BMI: %.2f\n", calculateBMI());
+   }
 }
